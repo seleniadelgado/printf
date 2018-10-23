fb28d429475aea18f676fa28fc3bf7470762a8da
scurry222 <scurry222@gmail.com>
2018-10-23 00:23:09 +0000

    fixed if statement, attempt to reference struct

---
+++ 0-printf.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * printf - function that produces output according to a format.
+ * @format - constant char format.
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+        consp green[] = {
+        {"c", print_c},
+        {"s", print_s},
+        {"pct", print_pct},
+        {"d", print_d},
+        {"i", print_i},
+        {NULL, NULL}
+        };
+
+        va_list args;
+
+        unsigned int i = 0;
+        unsigned int j = 0;
+
+        va_start(args format);
+        while ((format != NULL) && (format[i] != '\0'))
+        {
+                if (format[i] == '%')
+
+                        while (consp[j] < 5)
+                        {
+                                if (format[i + 1] == *(green[j].prin))
+                                {
+                                        (write(1, format, sizeof(format)));
+
+                                        type[j].type(list);
+
+                                }
+                                else
+                                        j++;
+
+                        }
+                if (j = 5)
+                        (write(1, error, sizeof(format)));
+
+                i++;
+        }
+        va_end(args)
+}
