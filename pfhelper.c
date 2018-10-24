eb2efaa77ca30c89f45bf94908cf1f6ca2e254f1
scurry222 <scurry222@gmail.com>
2018-10-24 16:52:59 +0000

    fixing github issues, removing pct

--- pfhelper.c
+++ pfhelper.c
@@ -30,34 +30,15 @@ int print_s(va_list args)
 	int i;
 
 	i = 0;
-<<<<<<< HEAD
 	*s = va_arg(args, int);
-=======
 	s = va_arg(args, char *);
->>>>>>> 978813a1e482f0c08681c24cb83e0c5380fc2d43
 	if (s == NULL)
 		return (0);
 	while (s[i] != '\0')
 		i++;
-<<<<<<< HEAD
 	write (1, &s, sizeof(char *));
-=======
 	write(1, s, i);
 		return (i);
-}
-/**
- * print_pct - function that prints '%' symbol.
- * @args: arguments passed to the function.
- * Return: returns a %.
- */
-int print_pct(va_list args)
-{
-	char *p;
-
-	p = va_arg(args, char *);
-	write(1, p, sizeof(char));
->>>>>>> 978813a1e482f0c08681c24cb83e0c5380fc2d43
-		return (0);
 }
 /**
  * print_d - function parameter to print a digit.
